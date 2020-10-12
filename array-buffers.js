8b54f75a6b5399eac4ce3f418a0c46919bf000c3
jsonLiang <yuanyu90221@gmail.com>
2020-10-12 11:39:11 +0800

    offical buffer

--- array-buffers.js
+++ array-buffers.js
@@ -1,5 +1,8 @@
 const data = Number(process.argv[2]);
-const u32Arr = new Uint32Array([data]);
-const u8Arr = new Uint8Array(u32Arr.buffer); 
-const u16Arr = new Uint16Array(u8Arr.buffer);
+const u32Arr = new Uint32Array(1);
+u32Arr[0] = data;
+const u16Arr = new Uint16Array(u32Arr.buffer);
+//const u32Arr = new Uint32Array([data]);
+//const u8Arr = new Uint8Array(u32Arr.buffer); 
+//const u16Arr = new Uint16Array(u8Arr.buffer);
 console.log(JSON.stringify(u16Arr));
