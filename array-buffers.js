4d477281704c24ad993ca9c2d179096d00fc22f5
jsonLiang <yuanyu90221@gmail.com>
2020-10-12 11:34:39 +0800

    my solution

---
+++ array-buffers.js
@@ -0,0 +1,5 @@
+const data = Number(process.argv[2]);
+const u32Arr = new Uint32Array([data]);
+const u8Arr = new Uint8Array(u32Arr.buffer); 
+const u16Arr = new Uint16Array(u8Arr.buffer);
+console.log(JSON.stringify(u16Arr));
